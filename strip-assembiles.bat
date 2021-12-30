b30ddb2597f6497011d0a29a6f1eae86b9df9188
Jacopo Libè <jacopo.libe@gmail.com>
2021-12-30 15:03:19 +0100

    Remove system libraries

--- strip-assembiles.bat
+++ strip-assembiles.bat
@@ -1,6 +1,7 @@
 @echo off 
 
 set toPublicize=Assembly-CSharp.dll assembly_valheim.dll assembly_utils.dll assembly_postprocessing.dll assembly_sunshafts.dll assembly_steamworks.dll assembly_simplemeshcombine.dll assembly_lux.dll assembly_guiutils.dll assembly_googleanalytics.dll
+set toIgnore=Mono.Security.dll mscorlib.dll System.Configuration.dll System.Core.dll System.dll System.Xml.dll
 
 set exePath=%1
 echo exePath: %exePath% 
@@ -21,6 +22,13 @@ set outPath=%~dp0\package\lib
   echo a: %%a
 
   %~dp0\tools\NStrip.exe "%managedPath%\%%a" -o "%outPath%\%%a" -cg -p --cg-exclude-events
+))
+
+@REM Removing unused packages
+(for %%a in (%toIgnore%) do (
+  echo a: %%a
+
+  del /f "%outPath%\%%a"
 ))
 
 pause
